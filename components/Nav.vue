dc5b398fdf8824bf7140f7b8914a09034800b02f
dalenysivilay <dalenysivilay@gmail.com>
2022-01-21 16:59:38 -0600

    Sass added

---
+++ components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="nav">
+    <div class="nav-wrapper">
+        <div class="logo">
+            <p>Daleny Sivilay</p>
+        </div>
+        <div class="nav-links">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Blog</a>
+            <a href="">Projects</a>
+            <a href="">Tools</a>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: Nav
+}
+</script>
+<style>
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,10 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'bulma',
+    '~/asets/css/main.css',
+    '~/assets/css/main.scss',
+    '~/assets/css/_variables.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
