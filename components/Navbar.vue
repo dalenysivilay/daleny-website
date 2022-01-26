35f285c19fb5a41d2bc2cfe8979f9a7bfddeba97
dalenysivilay <dalenysivilay@gmail.com>
2022-01-25 18:04:17 -0600

    navbar updated

---
+++ components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="nav">
+    <div class="nav-wrapper">
+        <div class="logo">
+            <p>Daleny Sivilay</p>
+        </div>
+        <div class="nav-links">
+            <ul>
+                <li>
+                     <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/blog">About</a>
+                </li>
+                <li>
+                    <a href="/projects">About</a>
+                </li>
+                <li>
+                    <a href="/tools">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: Navbar
+}
+</script>
+<style>
+</style>
